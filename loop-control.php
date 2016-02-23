9ed56f8c1b977b00e633316d56afd59889dbe050
millidavids <david.yurek@realpage.com>
2016-02-23 12:12:57 -0500

    divisble by 7, divisible by 10, first and last

--- loop-control.php
+++ loop-control.php
@@ -4,15 +4,13 @@ $numbers = array();
 foreach (range(0, 100, 3) as $i) {array_push($numbers, $i);}
 
 foreach ($numbers as $num) {
-    switch ($num) {
-    case 3:
-        echo str_repeat('Three', 1);
-        break;
-    case 9:
-        echo str_repeat('Nine', 3);
-        break;
-    case 15:
-        echo str_repeat('Fifteen', 5);
-        break;
+    if (array_search($num, $numbers) == 0) {
+        echo "First number.\n";
+    } elseif (array_search($num, $numbers) == sizeof($numbers) - 1) {
+        echo "Last number.\n";
+    } elseif ($num % 7 == 0) {
+        echo sprintf("Sevens are lucky, the number has %d.\n", $num / 7);
+    } elseif ($num % 10 == 0) {
+        echo "{$num} is a round number.\n";
     }
 }
