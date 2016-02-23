71852e654489322af5cc4228137a0f892e1248b9
millidavids <david.yurek@realpage.com>
2016-02-23 13:37:53 -0500

    class constants and doOperation private method

--- math.php
+++ math.php
@@ -1,19 +1,37 @@
 <?php
 
 class Math {
+
+    const ADD = 'add';
+    const SUBTRACT = 'subtract';
+    const MULTIPLY = 'multiply';
+    const DIVIDE = 'divide';
+
     public function add($x, $y, ...$zs) {
-        return $x + $y + array_sum($zs);
+        return $this->doOperation(self::ADD, $x, $y, $zs);
     }
 
     public function subtract($x, $y, ...$zs) {
-        return $x - $y - array_sum($zs);
+        return $this->doOperation(self::SUBTRACT, $x, $y, $zs);
     }
 
     public function multiply($x, $y, ...$zs) {
-        return $x * $y * array_product($zs);
+        return $this->doOperation(self::MULTIPLY, $x, $y, $zs);
     }
 
     public function divide($x, $y, ...$zs) {
-        return $x / $y / array_product($zs);
+        return $this->doOperation(self::DIVIDE, $x, $y, $zs);
+    }
+
+    private function doOperation($op, $x, $y, $zs) {
+        if ($op == self::ADD) {
+            return $x + $y + array_sum($zs);
+        } elseif ($op == self::SUBTRACT) {
+            return $x - $y - array_sum($zs);
+        } elseif ($op == self::MULTIPLY) {
+            return $x * $y * array_product($zs);
+        } elseif ($op == self::DIVIDE) {
+            return $x / $y / array_product($zs);
+        }
     }
 }
